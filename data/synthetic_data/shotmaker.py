f7406fd1a00bec4ea27b6ad429f40dea85a8d3c2
tre3x <gangulyofficialshreyan@gmail.com>
2021-06-08 21:44:11 +0530

    Initial commit

---
+++ data/synthetic_data/shotmaker.py
@@ -0,0 +1,84 @@
+import cv2
+import numpy as np
+import os
+
+class shotgenerator():
+    def __init__(self, vidname, fps, threshold_frame = 80):
+        '''
+        This class generates and stores individual shots of a video given the following 
+        inputs : 
+        INPUT : vidname, fps, thereshold_frame
+        vidname - Video filename according to the TRECVID IACC.3 dataset
+        fps : Set the Frames per Second of the generated shot
+        threshold_frame - Set the threshold value for shot generation
+        '''
+        self.here = os.path.dirname(os.path.abspath(__file__))
+        self.check_dir()
+        self.outpath = os.path.join(self.here, "shots", vidname.split('._-o-_.')[1].rsplit('.', 1)[0])
+        self.fps = fps
+        self.threshold_frame = threshold_frame
+        if not os.path.exists(self.outpath):
+            os.makedirs(self.outpath)
+            self.run(vidname)
+
+    def check_dir(self):
+        ########################################################
+        #Create result directory where video shots will be stored
+        ########################################################
+        shotspath = os.path.join(self.here, "shots")
+        if not os.path.isdir(shotspath):
+            os.mkdir(shotspath)  
+        
+    def get_boundaries(self, timestamps): 
+        '''
+        Returns timestamps of video in list format given the msb timestamp file
+        INPUT : timestamps
+        timestamps - msb timestamp file path of the concerened video.
+        OUPUT : times
+        times - a list containg timestamps of the video in list format.
+        '''
+        timestamps = os.path.splitext(timestamps)[0] + '.msb'
+        with open(timestamps, encoding = "ISO-8859-1") as f:
+            times = f.readlines()[2:]
+        f.close()    
+        return times
+    
+    def shotmaker(self, start, end, videopath):
+        '''
+        Generates and stores individual shots in a video given start and end frame
+        of the shots, and the videopath of the parent video. Shots are named with
+        the starting frame of the shot.
+        '''
+        video = cv2.VideoCapture(videopath)
+        framecount = start
+        height = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        width = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
+        outpath = os.path.join(self.outpath, str(start) + '.avi')
+        out = cv2.VideoWriter(outpath,cv2.VideoWriter_fourcc(*'XVID'), self.fps, (width, height))
+        while framecount <= end:
+            video.set(1, framecount-1)
+            res, frame = video.read()
+            out.write(frame)
+            framecount = framecount + 1
+        out.release()
+
+    def run(self, vidname):
+        '''
+        Check if the length of individual shots are greater than a threshold value.
+        If yes, runs the shotmaker function to generate and store shots. 
+        '''
+        timestamps = os.path.join(self.here, 'msb', vidname)
+        videopath = os.path.join(self.here, "videos", vidname.split('._-o-_.')[1])
+
+        times = self.get_boundaries(timestamps)
+        count = 1
+        for boundary in times:
+            print("######GENERATING SHOT " + str(count) + "######")
+            start = int(boundary.split(" ")[0])
+            end = int(boundary.split(" ")[1][:-1])
+            if (end - start) > self.threshold_frame:
+                self.shotmaker(start, end, videopath)   
+                count+=1
+
+if __name__ == '__main__':
+    shotgenerator("Political_videos-GeorgeWBush20020321_6_312._-o-_.Political_videos-GeorgeWBush20020321_6_312_512kb.mp4", 24)
---
+++ data/synthetic_data/viddownload.py
@@ -0,0 +1,69 @@
+import os
+import xml.etree.ElementTree as ET
+import urllib.request
+import random
+from shotmaker import shotgenerator
+
+'''
+Download videos in video folder and calls shotmaker.py to
+create shots.
+'''
+
+
+class downloader():
+    def __init__(self, filepath, num):
+        '''
+        This class downloads 'num' number of random videos and call the shotmaker
+        module for generatig individual shots.
+        INPUT : filepath, num
+        filepath - path of TRECVID IACC.3 dataset xml file
+        num - number of random videos to be downloaded and broken into shots
+        '''
+        tree = ET.parse(filepath)
+        self.here = os.path.dirname(os.path.abspath(__file__))
+        self.root = tree.getroot()
+        self.create_dir()
+        self.video_selector(num)
+
+    def create_dir(self):
+        ########################################################
+        #Create result directory where videos will be downloaded
+        ########################################################
+        videospath = os.path.join(self.here, "videos")
+        if not os.path.isdir(videospath):
+            os.mkdir(videospath)
+
+    def video_downloader(self, index):
+        '''
+        Download videos given the index of the video file in  TRECVID IACC.3 dataset.
+        Also calls the shotmaker module to make break and store the downloaded video
+        into individual shots
+        '''
+        filename = self.root[index][1].text
+        filesource = self.root[index][3].text
+        link = filesource + "/" + filename.split('._-o-_.')[1]
+        videopath = os.path.join(self.here, "videos", filename.split('._-o-_.')[1])
+        if not os.path.exists(videopath):
+            try:
+                urllib.request.urlretrieve(link, videopath) 
+                shotgenerator(filename, 24)
+            except:
+                pass
+
+    def video_selector(self, num):
+        ########################################################################
+        #Selects 'num' number of random video index from TRECVID IACC.3 dataset.
+        ########################################################################
+        index = random.sample(range(0, len(self.root)-1), num)
+        count = 1
+        for ind in index:
+            print("######DOWNLOADING VIDEO " + str(count) + "######")
+            print("This may take a while.")
+            self.video_downloader(ind)
+            count += 1
+
+if __name__ == '__main__':
+    downloader('iacc.3.collection.xml', 2)
+    
+
+entrevista_ulises__IABMexico_512kb
