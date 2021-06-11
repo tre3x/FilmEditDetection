d13bf8875829cb08f1de4bea26de34dc3ba80308
tre3x <gangulyofficialshreyan@gmail.com>
2021-06-12 03:13:14 +0530

    Added data generator script

---
+++ data/synthetic_data/run.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+XML_PATH='iacc.3.collection.xml'
+MSB_PATH='/home/tre3x/Python/FilmEditsDetection/data/synthetic_data/msb'
+declare -i VID_DOWNLOAD=2
+declare -i REQ_FPS=24
+declare -i NUM_HARDCUTS=10
+declare -i NUM_SOFTCUTS=10
+declare -i SOFTCUT_DURN=1
+declare -i REQ_FRAM=50
+
+function generate_data
+{
+echo 'INITIATING MODULES'
+python - <<START
+import viddownload, cutgenerator, finaldata
+viddownload.downloader("$XML_PATH", "$MSB_PATH", $VID_DOWNLOAD)
+cutgenerator.runner($NUM_HARDCUTS, $NUM_SOFTCUTS, $REQ_FPS, $SOFTCUT_DURN)
+finaldata.reader($REQ_FPS, $REQ_FRAM, False).run()
+finaldata.reader($REQ_FPS, $REQ_FRAM, True).run()
+START
+echo 'DATA SUCCESSFULLY GENERATED'
+} 
+
+
+generate_data
